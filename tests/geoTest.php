03a0f96ca3c6f42240981272ad4dd2b5616d48d8
smithroman83 <smithroman83@gmail.com>
2019-03-10 23:06:35 +0300

    Add new tests

--- tests/geoTest.php
+++ tests/geoTest.php
@@ -23,4 +23,40 @@ class geoTest extends TestCase
         $geo = new geo();
         $this->assertIsNumeric($geo->solution($geoArr));
     }
+
+    public function testDefaultTest()
+    {
+        $geoArr = [1,3,2,1,2,1,5,3,3,4,2];
+        $geo = new geo();
+        $this->assertEquals($geo->solution($geoArr),2);
+    }
+
+    public function testPeakTest()
+    {
+        $geoArr = [1,2,3,4,5,4,3,2,1];
+        $geo = new geo();
+        $this->assertEquals($geo->solution($geoArr),0);
+    }
+
+    public function testOther1Test()
+    {
+        $geoArr = [2,1,6,5,6,3,7,5,8];
+        $geo = new geo();
+        $this->assertEquals($geo->solution($geoArr),3);
+    }
+
+    public function testOther2Test()
+    {
+        $geoArr = [8,5,7,3,6,5,6,1,2];
+        $geo = new geo();
+        $this->assertEquals($geo->solution($geoArr),3);
+    }
+
+    public function testOther3Test()
+    {
+        $geoArr = [2,1,4,8,4,8,4,1,2];
+        $geo = new geo();
+        $this->assertEquals($geo->solution($geoArr),4);
+    }
+
 }
